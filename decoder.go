bd156e75c3438e4dbf6fc7b9a787a2b7643b90ee
David Findley <david@findley.dev>
2024-12-05 00:30:52 -0600

    feat: add multistream decoder API

--- decoder.go
+++ decoder.go
@@ -12,6 +12,7 @@ import (
 /*
 #cgo pkg-config: opus
 #include <opus.h>
+#include <opus/opus_multistream.h>
 
 int
 bridge_decoder_get_last_packet_duration(OpusDecoder *st, opus_int32 *samples)
@@ -31,11 +32,39 @@ type Decoder struct {
 	channels    int
 }
 
+type MSDecoder struct {
+	p *C.struct_OpusMSDecoder
+	// Same purpose as encoder struct
+	mem         []byte
+	sample_rate int
+	channels    int
+	streams int
+	coupled_streams int
+	mapping []byte
+}
+
 // NewDecoder allocates a new Opus decoder and initializes it with the
 // appropriate parameters. All related memory is managed by the Go GC.
 func NewDecoder(sample_rate int, channels int) (*Decoder, error) {
 	var dec Decoder
 	err := dec.Init(sample_rate, channels)
+	if err != nil {
+		return nil, err
+	}
+	return &dec, nil
+}
+
+// NewMSDecoder allocates a new Opus Multistream decoder and initializes it
+// with the appropriate parameters. All related memory is managed by the Go GC.
+func NewMSDecoder(
+	sample_rate int,
+	channels int,
+	streams int,
+	coupledStreams int,
+	mapping []byte,
+) (*MSDecoder, error) {
+	var dec MSDecoder
+	err := dec.Init(sample_rate, channels, streams, coupledStreams, mapping)
 	if err != nil {
 		return nil, err
 	}
@@ -58,6 +87,39 @@ func (dec *Decoder) Init(sample_rate int, channels int) error {
 		dec.p,
 		C.opus_int32(sample_rate),
 		C.int(channels))
+	if errno != 0 {
+		return Error(errno)
+	}
+	return nil
+}
+
+func (dec *MSDecoder) Init(
+	sample_rate int,
+	channels int,
+	streams int,
+	coupledStreams int,
+	mapping []byte,
+) error {
+	if dec.p != nil {
+		return fmt.Errorf("opus decoder already initialized")
+	}
+	size := C.opus_multistream_decoder_get_size(C.int(channels), C.int(coupledStreams))
+	dec.sample_rate = sample_rate
+	dec.channels = channels
+	dec.streams = streams
+	dec.coupled_streams = coupledStreams
+	dec.mapping = mapping
+	dec.mem = make([]byte, size)
+	dec.p = (*C.OpusMSDecoder)(unsafe.Pointer(&dec.mem[0]))
+	mappingPtr := (*C.uchar)(unsafe.Pointer(&mapping[0]))
+	errno := C.opus_multistream_decoder_init(
+		dec.p,
+		C.opus_int32(sample_rate),
+		C.int(channels),
+		C.int(streams),
+		C.int(coupledStreams),
+		mappingPtr,
+	)
 	if errno != 0 {
 		return Error(errno)
 	}
@@ -94,6 +156,35 @@ func (dec *Decoder) Decode(data []byte, pcm []int16) (int, error) {
 
 // Decode encoded Opus data into the supplied buffer. On success, returns the
 // number of samples correctly written to the target buffer.
+func (dec *MSDecoder) Decode(data []byte, pcm []int16) (int, error) {
+	if dec.p == nil {
+		return 0, errDecUninitialized
+	}
+	if len(data) == 0 {
+		return 0, fmt.Errorf("opus: no data supplied")
+	}
+	if len(pcm) == 0 {
+		return 0, fmt.Errorf("opus: target buffer empty")
+	}
+	if cap(pcm)%dec.channels != 0 {
+		return 0, fmt.Errorf("opus: target buffer capacity must be multiple of channels")
+	}
+
+	n := int(C.opus_multistream_decode(
+		dec.p,
+		(*C.uchar)(&data[0]),
+		C.opus_int32(len(data)),
+		(*C.opus_int16)(&pcm[0]),
+		C.int(cap(pcm)/dec.channels),
+		0))
+	if n < 0 {
+		return 0, Error(n)
+	}
+	return n, nil
+}
+
+// Decode encoded Opus data into the supplied buffer. On success, returns the
+// number of samples correctly written to the target buffer.
 func (dec *Decoder) DecodeFloat32(data []byte, pcm []float32) (int, error) {
 	if dec.p == nil {
 		return 0, errDecUninitialized
@@ -108,6 +199,35 @@ func (dec *Decoder) DecodeFloat32(data []byte, pcm []float32) (int, error) {
 		return 0, fmt.Errorf("opus: target buffer capacity must be multiple of channels")
 	}
 	n := int(C.opus_decode_float(
+		dec.p,
+		(*C.uchar)(&data[0]),
+		C.opus_int32(len(data)),
+		(*C.float)(&pcm[0]),
+		C.int(cap(pcm)/dec.channels),
+		0))
+	if n < 0 {
+		return 0, Error(n)
+	}
+	return n, nil
+}
+
+// Decode encoded Opus data into the supplied buffer. On success, returns the
+// number of samples correctly written to the target buffer.
+func (dec *MSDecoder) DecodeFloat32(data []byte, pcm []float32) (int, error) {
+	if dec.p == nil {
+		return 0, errDecUninitialized
+	}
+	if len(data) == 0 {
+		return 0, fmt.Errorf("opus: no data supplied")
+	}
+	if len(pcm) == 0 {
+		return 0, fmt.Errorf("opus: target buffer empty")
+	}
+	if cap(pcm)%dec.channels != 0 {
+		return 0, fmt.Errorf("opus: target buffer capacity must be multiple of channels")
+	}
+
+	n := int(C.opus_multistream_decode_float(
 		dec.p,
 		(*C.uchar)(&data[0]),
 		C.opus_int32(len(data)),
@@ -161,6 +281,47 @@ func (dec *Decoder) DecodeFEC(data []byte, pcm []int16) error {
 	return nil
 }
 
+// DecodeFEC encoded Opus data into the supplied buffer with forward error
+// correction.
+//
+// It is to be used on the packet directly following the lost one.  The supplied
+// buffer needs to be exactly the duration of audio that is missing
+//
+// When a packet is considered "lost", DecodeFEC can be called on the next
+// packet in order to try and recover some of the lost data. The PCM needs to be
+// exactly the duration of audio that is missing.  `LastPacketDuration()` can be
+// used on the decoder to get the length of the last packet.  Note also that in
+// order to use this feature the encoder needs to be configured with
+// SetInBandFEC(true) and SetPacketLossPerc(x) options.
+//
+// Note that DecodeFEC automatically falls back to PLC when no FEC data is
+// available in the provided packet.
+func (dec *MSDecoder) DecodeFEC(data []byte, pcm []int16) error {
+	if dec.p == nil {
+		return errDecUninitialized
+	}
+	if len(data) == 0 {
+		return fmt.Errorf("opus: no data supplied")
+	}
+	if len(pcm) == 0 {
+		return fmt.Errorf("opus: target buffer empty")
+	}
+	if cap(pcm)%dec.channels != 0 {
+		return fmt.Errorf("opus: target buffer capacity must be multiple of channels")
+	}
+	n := int(C.opus_multistream_decode(
+		dec.p,
+		(*C.uchar)(&data[0]),
+		C.opus_int32(len(data)),
+		(*C.opus_int16)(&pcm[0]),
+		C.int(cap(pcm)/dec.channels),
+		1))
+	if n < 0 {
+		return Error(n)
+	}
+	return nil
+}
+
 // DecodeFECFloat32 encoded Opus data into the supplied buffer with forward error
 // correction. It is to be used on the packet directly following the lost one.
 // The supplied buffer needs to be exactly the duration of audio that is missing
@@ -178,6 +339,35 @@ func (dec *Decoder) DecodeFECFloat32(data []byte, pcm []float32) error {
 		return fmt.Errorf("opus: target buffer capacity must be multiple of channels")
 	}
 	n := int(C.opus_decode_float(
+		dec.p,
+		(*C.uchar)(&data[0]),
+		C.opus_int32(len(data)),
+		(*C.float)(&pcm[0]),
+		C.int(cap(pcm)/dec.channels),
+		1))
+	if n < 0 {
+		return Error(n)
+	}
+	return nil
+}
+
+// DecodeFECFloat32 encoded Opus data into the supplied buffer with forward error
+// correction. It is to be used on the packet directly following the lost one.
+// The supplied buffer needs to be exactly the duration of audio that is missing
+func (dec *MSDecoder) DecodeFECFloat32(data []byte, pcm []float32) error {
+	if dec.p == nil {
+		return errDecUninitialized
+	}
+	if len(data) == 0 {
+		return fmt.Errorf("opus: no data supplied")
+	}
+	if len(pcm) == 0 {
+		return fmt.Errorf("opus: target buffer empty")
+	}
+	if cap(pcm)%dec.channels != 0 {
+		return fmt.Errorf("opus: target buffer capacity must be multiple of channels")
+	}
+	n := int(C.opus_multistream_decode_float(
 		dec.p,
 		(*C.uchar)(&data[0]),
 		C.opus_int32(len(data)),
@@ -225,6 +415,41 @@ func (dec *Decoder) DecodePLC(pcm []int16) error {
 	return nil
 }
 
+// DecodePLC recovers a lost packet using Opus Packet Loss Concealment feature.
+//
+// The supplied buffer needs to be exactly the duration of audio that is missing.
+// When a packet is considered "lost", `DecodePLC` and `DecodePLCFloat32` methods
+// can be called in order to obtain something better sounding than just silence.
+// The PCM needs to be exactly the duration of audio that is missing.
+// `LastPacketDuration()` can be used on the decoder to get the length of the
+// last packet.
+//
+// This option does not require any additional encoder options. Unlike FEC,
+// PLC does not introduce additional latency. It is calculated from the previous
+// packet, not from the next one.
+func (dec *MSDecoder) DecodePLC(pcm []int16) error {
+	if dec.p == nil {
+		return errDecUninitialized
+	}
+	if len(pcm) == 0 {
+		return fmt.Errorf("opus: target buffer empty")
+	}
+	if cap(pcm)%dec.channels != 0 {
+		return fmt.Errorf("opus: output buffer capacity must be multiple of channels")
+	}
+	n := int(C.opus_multistream_decode(
+		dec.p,
+		nil,
+		0,
+		(*C.opus_int16)(&pcm[0]),
+		C.int(cap(pcm)/dec.channels),
+		0))
+	if n < 0 {
+		return Error(n)
+	}
+	return nil
+}
+
 // DecodePLCFloat32 recovers a lost packet using Opus Packet Loss Concealment feature.
 // The supplied buffer needs to be exactly the duration of audio that is missing.
 func (dec *Decoder) DecodePLCFloat32(pcm []float32) error {
@@ -238,6 +463,31 @@ func (dec *Decoder) DecodePLCFloat32(pcm []float32) error {
 		return fmt.Errorf("opus: output buffer capacity must be multiple of channels")
 	}
 	n := int(C.opus_decode_float(
+		dec.p,
+		nil,
+		0,
+		(*C.float)(&pcm[0]),
+		C.int(cap(pcm)/dec.channels),
+		0))
+	if n < 0 {
+		return Error(n)
+	}
+	return nil
+}
+
+// DecodePLCFloat32 recovers a lost packet using Opus Packet Loss Concealment feature.
+// The supplied buffer needs to be exactly the duration of audio that is missing.
+func (dec *MSDecoder) DecodePLCFloat32(pcm []float32) error {
+	if dec.p == nil {
+		return errDecUninitialized
+	}
+	if len(pcm) == 0 {
+		return fmt.Errorf("opus: target buffer empty")
+	}
+	if cap(pcm)%dec.channels != 0 {
+		return fmt.Errorf("opus: output buffer capacity must be multiple of channels")
+	}
+	n := int(C.opus_multistream_decode_float(
 		dec.p,
 		nil,
 		0,
